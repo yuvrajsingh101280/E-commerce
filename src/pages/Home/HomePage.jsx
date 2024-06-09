9bcf0757070c149f2d285d04e51d5fd270d62457
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-06-09 12:11:03 +0530

    product card added in the home page

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Category from "../../components/Category/Category";
+import HomepageProducCard from "../../components/homePageProductCard/HomepageProducCard";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <Category />
+      <HomepageProducCard />
     </div>
   );
 };
