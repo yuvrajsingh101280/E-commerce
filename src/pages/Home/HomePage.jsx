e88f00127181890030215e85c8f11405286b6ed9
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-06-09 10:49:54 +0530

    created category section

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../../components/heroSection/HeroSection";
+import Category from "../../components/Category/Category";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <Category />
     </div>
   );
 };
