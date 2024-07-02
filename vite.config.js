b1323b88cf8f5908ced6b466ddee872508b177f0
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-07-02 08:44:42 +0530

    build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'lucide-react': path.resolve(__dirname, 'node_modules/lucide-react')
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['lucide-react']
+    }
+  }
+});
