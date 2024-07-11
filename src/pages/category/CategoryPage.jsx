54a5889c155527dea0525a26b50ad981e93fbb6c
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-07-11 22:13:12 +0530

    category page completed

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,13 +1,96 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from "react-router";
+
+import { useContext } from "react";
+import myContext from "../../context/MyContext";
+import Loader from "../../components/loader/Loader";
 
 const CategoryPage = () => {
-const{categoryname} = useParams()
-  return (
-    <div>
-      category {categoryname}
-    </div>
-  )
-}
+  const { categoryname } = useParams();
 
-export default CategoryPage
+  const context = useContext(myContext);
+  const { getAllProduct, loading } = context;
+
+  const navigate = useNavigate();
+
+  // filter product
+  const filterProduct = getAllProduct.filter((obj) =>
+    obj.category.includes(categoryname)
+  );
+  // console.log(filterProduct)
+  return (
+    <div className="mt-10">
+      {/* Heading  */}
+      <div className="">
+        <h1 className=" text-center mb-5 text-2xl font-semibold first-letter:uppercase">
+          {categoryname}
+        </h1>
+      </div>
+
+      {loading ? (
+        <div className="flex justify-center">
+          <Loader />
+        </div>
+      ) : (
+        <section className="text-gray-600 body-font">
+          {/* main 2 */}
+          <div className="container px-5 py-5 mx-auto">
+            {/* main 3  */}
+            <div className="flex flex-wrap -m-4 justify-center">
+              {filterProduct.length > 0 ? (
+                <>
+                  {filterProduct.map((item, index) => {
+                    const { id, title, price, productImageUrl } = item;
+                    return (
+                      <div key={index} className="p-4 w-full md:w-1/4">
+                        <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
+                          <img
+                            onClick={() => navigate(`/productinfo/${id}`)}
+                            className="lg:h-80  h-96 w-full"
+                            src={productImageUrl}
+                            alt="img"
+                          />
+                          <div className="p-6">
+                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                              E-bharat
+                            </h2>
+                            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                              {title.substring(0, 25)}
+                            </h1>
+                            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                              ₹{price}
+                            </h1>
+
+                            <div className="flex justify-center ">
+                              <button className=" bg-blue-500 hover:bg-blue-600 w-full text-white py-[4px] rounded-lg font-bold">
+                                Add To Cart
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </>
+              ) : (
+                <div>
+                  <div className="flex justify-center">
+                    <img
+                      className=" mb-2"
+                      src="https://cdn-icons-png.flaticon.com/128/2748/2748614.png"
+                      alt=""
+                    />
+                  </div>
+                  <h1 className=" text-black text-xl">
+                    No {categoryname} product found
+                  </h1>
+                </div>
+              )}
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default CategoryPage;
